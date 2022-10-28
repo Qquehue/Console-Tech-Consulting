b2c0994903daf20a70ccc03f1820c09244ad69ab
hessel-lu <lucas.hessel@sptech.school>
2022-10-28 19:48:33 -0300

    ajuste na senha do database - config.js

--- site/src/database/config.js
+++ site/src/database/config.js
@@ -22,7 +22,7 @@ var mySqlConfig = {
     host: "localhost",
     database: "ctc",
     user: "root",
-    password: "10091994Manager*",
+    password: "urubu100",
 };
 
 function executar(instrucao) {
