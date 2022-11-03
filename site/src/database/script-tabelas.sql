95e1f749bc77d2fcf98401fc4e054181d5067466
MatheusNasci <matheus.noliveira@sptech.school>
2022-11-03 19:40:09 -0300

    Validacoes

--- site/src/database/script-tabelas.sql
+++ site/src/database/script-tabelas.sql
@@ -1,22 +1,68 @@
-create database deep_abyss;
+CREATE DATABASE CTC; 
 
-use deep_abyss;
+USE CTC;  
 
-create table Usuario(
-idUsuario int primary key auto_increment,
-nome varchar(45),
-email varchar(45),
-telefone char (11),
-cpf char (11),
-senha varchar(15)
-);
+CREATE TABLE Linha (
+idLinha INT PRIMARY KEY AUTO_INCREMENT,  
+nomeLinha VARCHAR(45),  
+situacaoLinha CHAR(10)
+);  
 
-create table pontuacao(
-idPontuacao int auto_increment,
-fkUsuario int unique,
-foreign key (fkUsuario) references Usuario(idUsuario),
-primary key (idPontuacao, fkUsuario),
-acertos int,
-erros int,
-dt date
-);
+CREATE TABLE Estacao (  
+idEstacao INT PRIMARY KEY AUTO_INCREMENT,  
+nomeEstacao VARCHAR(45),  
+situacaoEstacao CHAR(10),  
+fkLinha INT,  
+FOREIGN KEY (fkLinha) REFERENCES Linha(idLinha)  
+);  
+
+CREATE TABLE Maquina (  
+idMaquina INT PRIMARY KEY AUTO_INCREMENT,  
+modeloCPU VARCHAR(45),  
+totalMemoria DOUBLE,  
+totalDisco DOUBLE,  
+dataCadastro DATETIME, 
+fkEstacao INT,  
+FOREIGN KEY (fkEstacao) REFERENCES Estacao(idEstacao)  
+);  
+
+CREATE TABLE UsoMaquina (  
+idUso INT PRIMARY KEY AUTO_INCREMENT, 
+temperaturaCPU DOUBLE, 
+usoCPU DOUBLE, 
+usoMemoria DOUBLE, 
+upTime DATETIME, 
+fkMaquina INT,  
+FOREIGN KEY (fkMaquina) REFERENCES Maquina(idMaquina)  
+);  
+
+CREATE TABLE Cargo (  
+idCargo INT PRIMARY KEY AUTO_INCREMENT,  
+nomeCargo VARCHAR(45)  
+);  
+
+CREATE TABLE Funcionario (  
+idFuncionario INT PRIMARY KEY AUTO_INCREMENT,  
+nomeFuncionario VARCHAR(45),  
+CPF CHAR(11),  
+telefone CHAR(15),  
+email VARCHAR(45),  
+senha VARCHAR(45),  
+fkLinha INT,  
+FOREIGN KEY (fkLinha) REFERENCES Linha(idLinha),  
+fkCargo INT,  
+FOREIGN KEY (fkCargo) REFERENCES Cargo(idCargo)  
+); 
+
+INSERT INTO Cargo (nomeCargo) VALUES 
+("Analista"), 
+("Tecnico"); 
+
+INSERT INTO Linha (nomeLinha) VALUES 
+("Azul"), 
+("Verde"), 
+("Vermelha"); 
+
+SELECT * FROM Cargo; 
+
+SELECT * FROM Linha; 
